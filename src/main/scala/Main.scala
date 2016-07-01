f4ba205605a5f0fb434e7094d26d6c32d56af998
L Dean <lgdean@users.noreply.github.com>
2016-07-01 14:28:55 -0700

    add some logging to help debug

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.http._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
@@ -10,16 +11,28 @@ trait HelloApp {
 
   import system.dispatcher
 
+  val log = Logging(system, getClass)
+
   val route =
     pathSingleSlash {
+      log.info("happy to serve you /")
       getFromResource("index.html");
     }
 
-  Http().bindAndHandle(
+  val host = "0.0.0.0"
+  val port = 8080
+  val http = Http().bindAndHandle(
     route,
-    "0.0.0.0",
-    8080
+    host,
+    port
   )
+
+  http onFailure {
+    case ex: Exception =>
+      log.error(ex, "Failed to bind to {}:{}", host, port)
+  }
+
+  log.info("the hello app has started")
 }
 
 object Main extends App with HelloApp {
